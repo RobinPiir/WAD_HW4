bd38cb4a91be78e7f16e6cb55a45150b3262b097
Yasuven <147263850+Yasuven@users.noreply.github.com>
2023-12-19 21:07:51 +0200

    Pretty contact us

--- frontend/src/components/ContactUsCompo.vue
+++ frontend/src/components/ContactUsCompo.vue
@@ -11,6 +11,30 @@
             EMAIL: Robin.piir@gmail.com<br>
             NUMBER: 05202500252</p>
     </div>
+    <div class="text1">
+        <h1>Contact Us</h1>
+        <p>Karolin Kerro: <br>
+            EMAIL: kerrokarolin@gmail.com<br>
+            NUMBER: 05202500252</p>
+        <p>Roland-Carlis Vares: <br>
+            EMAIL: rolts.vares@gmail.com<br>
+            NUMBER: 05202500252</p>
+        <p>Robin Piir: <br>
+            EMAIL: Robin.piir@gmail.com<br>
+            NUMBER: 05202500252</p>
+    </div>
+    <div class="text2">
+        <h1>Contact Us</h1>
+        <p>Karolin Kerro: <br>
+            EMAIL: kerrokarolin@gmail.com<br>
+            NUMBER: 05202500252</p>
+        <p>Roland-Carlis Vares: <br>
+            EMAIL: rolts.vares@gmail.com<br>
+            NUMBER: 05202500252</p>
+        <p>Robin Piir: <br>
+            EMAIL: Robin.piir@gmail.com<br>
+            NUMBER: 05202500252</p>
+    </div>
 </template>
 
 
@@ -29,6 +53,40 @@ export default {
     text-align: center;
     top: 50%;
     left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border: 1px solid #646174;
+    background-color: #b1cda4;
+    border-radius: 5px;
+}
+.text1 {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    text-align: center;
+    top: 80%;
+    left: 34%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border: 1px solid #646174;
+    background-color: #b1cda4;
+    border-radius: 5px;
+}
+.text2 {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    text-align: center;
+    top: 15%;
+    left: 15%;
     margin-right: -50%;
     transform: translate(-50%, -50%);
     padding-top: 10px;
