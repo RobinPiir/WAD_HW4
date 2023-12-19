d4612263a9010e6902d266ac92705b310031febc
M6msu <robin.piir@gmail.com>
2023-12-19 21:16:56 +0200

    fixed backend

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ app.use(express.json());
             console.log("a post request has arrived");
             const post = req.body;
             const newpost = await pool.query(
-                "INSERT INTO posttable(date, body, urllink) values ($1, $2, $3)    RETURNING*", [post.date, post.body, post.urllink]
-    // $1, $2, $3 are mapped to the first, second and third element of the passed array (post.title, post.body, post.urllink) 
+                "INSERT INTO posttable(date, body) values ($1, $2)    RETURNING*", [post.date, post.body]
+    // $1, $2, $3 are mapped to the first, second and third element of the passed array (post.title, post.body) 
     // The RETURNING keyword in PostgreSQL allows returning a value from the insert or update statement.
     // using "*" after the RETURNING keyword in PostgreSQL, will return everything
             );
